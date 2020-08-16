5bfa9ba69d40e2859ca04335dc4d6f0f0e9b37f2
sieugene <evgenii.sidelnikov@unitbean.io>
2020-08-16 12:32:47 +0300

    fs commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+const { sum } = require("./App")
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('should be sum', () => {
+  const value = sum(1,2)
+  expect(value).toBe(3);
+  
+})
+
